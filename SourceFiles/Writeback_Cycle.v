883c97e8be76409faaa74d5a948420538b2f24c8
Isagiyoichi79 <161978660+Isagiyoichi79@users.noreply.github.com>
2024-11-10 13:43:18 +0530

    Update Writeback_Cycle.v

--- SourceFiles/Writeback_Cycle.v
+++ SourceFiles/Writeback_Cycle.v
@@ -13,11 +13,6 @@ module writeback_cycle(
     output regwriteW,
     output [4:0] rdW
 );
-    // wire [31:0] result;
-    // reg [31:0] resultW_r;
-    // reg [4:0] rdW_r;
-    // reg regwriteW_r;
-
     // Mux instantiation to select between ALU result and memory data
     Mux writeback_mux (
         .selection_line(isloadW),
@@ -26,20 +21,6 @@ module writeback_cycle(
         .out(resultW)
     );
 
-    // Update resultW on the rising edge of clk or reset
-    // always @(posedge clk ) begin
-    //     if (rst) begin
-    //         resultW_r <= 32'h0;  // Reset resultW to 0
-    //         regwriteW_r <= 1'b0;
-    //         rdW_r <= 5'b00000;
-    //     end else begin
-    //         resultW_r <= result; // Assign the mux output to resultW
-    //         regwriteW_r <= regwrite;
-    //         rdW_r <= rd;
-    //     end
-    // end
-
-    // assign resultW = resultW_r;
     assign regwriteW = regwrite;
     assign rdW =rd;
 endmodule
