7185f68492936775f77ca253ddc8a550e5e53a4e
UTKARSH KUMAR <utkarsh_k@cs.iitr.ac.in>
2024-11-24 18:05:41 +0530

    Delete SourceFiles directory

--- SourceFiles/Adder.v
+++
@@ -1,5 +0,0 @@
-module Adder(input1,input2,out);
-    input [31:0] input1,input2;
-    output [31:0] out;
-    assign out=input1+input2;
-endmodule
--- SourceFiles/Shift_Left.v
+++
@@ -1,5 +0,0 @@
-module ShiftLeft(input [31:0] inData, output reg [31:0] outData);
-    always @(*) begin
-        outData = inData << 2;
-    end
-endmodule
